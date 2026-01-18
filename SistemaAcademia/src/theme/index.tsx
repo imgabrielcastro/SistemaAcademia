87c07f97023d4b336ce713f237777bc4e6b116cb
Gabriel Souza de Castro <110572481+imgabrielcastro@users.noreply.github.com>
2026-01-18 10:39:45 -0300

    Atualiza .gitignore e limpa cache

---
+++ SistemaAcademia/src/theme/index.tsx
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: 'system-ui, Avenir, Helvetica, Arial, sans-serif',
+    h4: {
+      fontWeight: 'bold',
+    },
+  },
+  palette: {
+    primary: {
+      main: '#833AB4',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+    background: {
+      default: '#EEEEEE',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+  },
+});
